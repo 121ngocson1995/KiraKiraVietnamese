d4c96d72f47e20d0a26820d13c94a79f8030ba10
minhbeo2805 <nguyenleminh2805@gmail.com>
2017-03-09 15:49:31 +0700

    Update P8 dùng database

--- resources/views/P8.blade.php
+++ resources/views/P8.blade.php
@@ -57,7 +57,7 @@
 	function next() {
 
 		if (checkAnswer(elementData , rightAnswerCnt) || checkQuestion == false) {
-
+			rightAnswerCnt = 0;
 			for (var i = 0; i < checkFinish.length; i++) {
 				if (checkFinish[i]['dialogNo'] == dialogNow) {
 					checkFinish[i]['finish'] = true;
@@ -170,144 +170,150 @@
 		dialogAnswer.sort(function(a, b){return 0.5 - Math.random()});
 		for (var i = 0; i < dialogAnswer.length; i++) {
 			for (var j = 0; j < dialogAnswer[i]['answer'].length; j++) {
-				var node = document.createElement("span");
-				node.setAttribute('draggable', 'true');
-				node.setAttribute('class', 'dragWord ui-state-default');
-				node.setAttribute('id', (dialogNow-1)+','+dialogAnswer[i]['lineNo']+','+j);
-				node.setAttribute('ondragstart', 'javascript: drag(event)');
-				var textnode = document.createTextNode(dialogAnswer[i]['answer'][j]);
-				node.appendChild(textnode);
-				document.getElementById("answer_id").appendChild(node);
-			}
-		}
-	}
+				if (dialogAnswer[i]['answer'][j].localeCompare("") != 0) {
+					var node = document.createElement("span");
+					node.setAttribute('draggable', 'true');
+					node.setAttribute('class', 'dragWord ui-state-default');
+					node.setAttribute('id', (dialogNow-1)+','+dialogAnswer[i]['lineNo']+','+j);
+					node.setAttribute('ondragstart', 'javascript: drag(event)');
+					var textnode = document.createTextNode(dialogAnswer[i]['answer'][j]);
+					node.appendChild(textnode);
+					document.getElementById("answer_id").appendChild(node);}
 
-
-	function editButtonGr(dialogCnt, dialogNow) {
-		while (document.getElementById("btn-group").firstChild) {
-			document.getElementById("btn-group").removeChild(document.getElementById("btn-group").firstChild);
-		}
-		for (var i = 1; i <= dialogCnt.length; i++) {
-			var node = document.createElement("button");
-			var textNode = document.createTextNode('D'+i);
-			node.setAttribute('id', i);
-			node.setAttribute('type', 'button');
-			node.setAttribute('class', 'btn btn-primary');
-			node.setAttribute('onclick', 'JavaScript: chooseD(this)');
-			node.appendChild(textNode);
-			if (i > dialogNow  ) {
-				node.setAttribute('disabled', 'true');
-			}
-
-			document.getElementById("btn-group").appendChild(node);
-		}
-
-	}
-
-	function checkAnswer(elementData , rightAnswerCnt){
-		var dialogAnswer = new Array();
-		var result;
-		for (var i = 0; i < elementData.length; i++) {
-			if (elementData[i]['dialogNo'] == dialogNow) {
-				for (var j = 0; j < elementData[i]['answer'].length; j++) {
-					dialogAnswer.push(elementData[i]['answer'][j]);
 				}
 			}
 		}
-		if (dialogAnswer.length == rightAnswerCnt) {
-			result = true;
-			rightAnswerCnt = 0;
-		}else{
-			for (var i = 0; i < dialogAnswer.length; i++) {
-				delete dialogAnswer[i];
+
+
+		function editButtonGr(dialogCnt, dialogNow) {
+			while (document.getElementById("btn-group").firstChild) {
+				document.getElementById("btn-group").removeChild(document.getElementById("btn-group").firstChild);
 			}
-			result = false;
+			for (var i = 1; i <= dialogCnt.length; i++) {
+				var node = document.createElement("button");
+				var textNode = document.createTextNode('D'+i);
+				node.setAttribute('id', i);
+				node.setAttribute('type', 'button');
+				node.setAttribute('class', 'btn btn-primary');
+				node.setAttribute('onclick', 'JavaScript: chooseD(this)');
+				node.appendChild(textNode);
+				if (i > dialogNow  ) {
+					node.setAttribute('disabled', 'true');
+				}
+
+				document.getElementById("btn-group").appendChild(node);
+			}
+
 		}
-		return result;
-	}
 
-	function allowDrop(event){
-		event.preventDefault();
-	}
+		function checkAnswer(elementData , rightAnswerCnt){
+			var dialogAnswer = new Array();
+			var result;
+			for (var i = 0; i < elementData.length; i++) {
+				if (elementData[i]['dialogNo'] == dialogNow) {
+					for (var j = 0; j < elementData[i]['answer'].length; j++) {
+						if (elementData[i]['answer'][j].localeCompare("")!=0) {
+							dialogAnswer.push(elementData[i]['answer'][j]);
+						}
+					}
+				}
+			}
+			console.log(dialogAnswer);
+			if (dialogAnswer.length == rightAnswerCnt) {
+				result = true;
+			}else{
+				for (var i = 0; i < dialogAnswer.length; i++) {
+					delete dialogAnswer[i];
+				}
+				result = false;
+			}
+			return result;
+		}
 
-	function drag(event) {
-		event.dataTransfer.setData("Text", event.target.childNodes[0].data);
-	}
+		function allowDrop(event){
+			event.preventDefault();
+		}
 
-	function drop(element, event) {
-		event.preventDefault();
-		var answerText = event.dataTransfer.getData("Text")
+		function drag(event) {
+			event.dataTransfer.setData("Text", event.target.getAttribute("id"));
+		}
 
-		var data = element.getAttribute('id').split(',');
-		var lineNo = data[0];
-		var answerOrder = data[1];
-		var rightAnswer;
+		function drop(element, event) {
+			event.preventDefault();
+			var targetId = event.dataTransfer.getData("Text")
+			var answerText = document.getElementById(targetId).innerHTML;
+			var data = element.getAttribute('id').split(',');
+			var lineNo = data[0];
+			var answerOrder = data[1];
+			var rightAnswer;
 
-		for (var i = 0; i < elementData.length; i++) {
-			if (elementData[i]['dialogNo'] == dialogNow && elementData[i]['lineNo'] == lineNo ) {
-				rightAnswer = elementData[i]['answer'];
+			for (var i = 0; i < elementData.length; i++) {
+				if (elementData[i]['dialogNo'] == dialogNow && elementData[i]['lineNo'] == lineNo ) {
+					rightAnswer = elementData[i]['answer'];
+				}
+			}
+
+			if (rightAnswer[answerOrder].localeCompare(answerText) == 0) {
+				element.innerHTML = answerText;
+				element.setAttribute('style', 'width: auto; height: auto; background-color:transparent; display: inline-block; font-weight: 500;');
+				document.getElementById(targetId).setAttribute('style', 'opacity: 0;');
+				document.getElementById(targetId).setAttribute('draggable', 'false');
+				rightAnswerCnt++;
 			}
 		}
-		
-		if (rightAnswer[answerOrder].localeCompare(answerText) == 0) {
-			element.innerHTML = answerText;
-			element.setAttribute('style', 'width: auto; height: auto; background-color:transparent; display: inline-block; font-weight: 500;');
-			document.getElementById((dialogNow-1)+','+lineNo+','+answerOrder).setAttribute('style', 'opacity: 0;');
-			document.getElementById((dialogNow-1)+','+lineNo+','+answerOrder).setAttribute('draggable', 'false');
-			rightAnswerCnt++;
-		}
-	}
 
-</script>
-<div id="btn-group" class="btn-group">
-	@for ($i = 1; $i <= count($dialogCnt); $i++)
-	<button id="{{$i}}" type="button" 
-	@if ($i > 1)
-	disabled="true" 
-	@endif class="btn btn-primary" onclick="JavaScript: chooseD(this)">D{{$i}}</button>
-	@endfor
-</div>
-<br>
-<div id="answer_id" style="width: auto; padding: 10px; height: 100px; background-color:white;">
-	@php
-	$dialogAnswer = array();
-	for ($i=0; $i < count($elementData) ; $i++) { 
-		if ($elementData[$i]->dialogNo == 1) {
-			array_push($dialogAnswer, $elementData[$i]);
-		}
-	}
-	shuffle($dialogAnswer);
-	@endphp
-	@for ($i = 0; $i < count($dialogAnswer) ; $i++)
-	@for ($j = 0; $j < count($dialogAnswer[$i]->answer) ; $j++)
-	<span id="0,{{$dialogAnswer[$i]->lineNo}},{{$j}}" ondragstart="javascript: drag(event)" draggable="true" class="dragWord ui-state-default">{{$dialogAnswer[$i]->answer[$j]}}</span>
-	@endfor	
-	@endfor
-</div>
-<div class="row">
-	<div id="content_id" class="col-sm-10 col-md-10 col-lg-10">
-		@for ($i = 0; $i < count($elementData) ; $i++)
-		@if ($elementData[$i]->dialogNo == 1)
+	</script>
+	<div id="btn-group" class="btn-group">
+		@for ($i = 1; $i <= count($dialogCnt); $i++)
+		<button id="{{$i}}" type="button" 
+		@if ($i > 1)
+		disabled="true" 
+		@endif class="btn btn-primary" onclick="JavaScript: chooseD(this)">D{{$i}}</button>
+		@endfor
+	</div>
+	<br>
+	<div id="answer_id" style="width: auto; padding: 10px; height: 100px; background-color:white;">
 		@php
-		$curline = explode('*', $elementData[$i]->line);
-		$index = 0;
+		$dialogAnswer = array();
+		for ($i=0; $i < count($elementData) ; $i++) { 
+			if ($elementData[$i]->dialogNo == 1) {
+				array_push($dialogAnswer, $elementData[$i]);
+			}
+		}
+		shuffle($dialogAnswer);
 		@endphp
-		<div style="font-size: 25px; padding: 15px">
-			@for ($j = 0; $j < count($curline) ; $j++)
-			@if ($index != count($curline)-1)
-			{{$curline[$j]}}<div id="{{$elementData[$i]->lineNo}},{{$j}}" style="width: 100px; height: 30px; background-color:green; display: inline-block; opacity: 0.1; font-weight: 500" ondragenter="return false;" ondragover="return false;" ondrop="drop(this,event)"></div>
+		@for ($i = 0; $i < count($dialogAnswer) ; $i++)
+		@for ($j = 0; $j < count($dialogAnswer[$i]->answer) ; $j++)
+		@if (strcmp($dialogAnswer[$i]->answer[$j], "") != 0 )
+		<span id="0,{{$dialogAnswer[$i]->lineNo}},{{$j}}" ondragstart="javascript: drag(event)" draggable="true" class="dragWord ui-state-default">{{$dialogAnswer[$i]->answer[$j]}}</span>
+		@endif
+		@endfor	
+		@endfor
+	</div>
+	<div class="row">
+		<div id="content_id" class="col-sm-10 col-md-10 col-lg-10">
+			@for ($i = 0; $i < count($elementData) ; $i++)
+			@if ($elementData[$i]->dialogNo == 1)
 			@php
-			$index++;
+			$curline = explode('*', $elementData[$i]->line);
+			$index = 0;
 			@endphp
-			@else
-			{{$curline[$j]}}
+			<div style="font-size: 25px; padding: 15px">
+				@for ($j = 0; $j < count($curline) ; $j++)
+				@if ($index != count($curline)-1)
+				{{$curline[$j]}}<div id="{{$elementData[$i]->lineNo}},{{$j}}" style="width: 100px; height: 30px; background-color:green; display: inline-block; opacity: 0.1; font-weight: 500" ondragenter="return false;" ondragover="return false;" ondrop="drop(this,event)"></div>
+				@php
+				$index++;
+				@endphp
+				@else
+				{{$curline[$j]}}
+				@endif
+				@endfor
+			</div>
 			@endif
 			@endfor
 		</div>
-		@endif
-		@endfor
+		<div class="col-sm-2 col-md-2 col-lg-2"><button type="button" class="btn btn-primary" onclick="JavaScript: next()">Next</button>
+		</div>
 	</div>
-	<div class="col-sm-2 col-md-2 col-lg-2"><button type="button" class="btn btn-primary" onclick="JavaScript: next()">Next</button>
-	</div>
-</div>
-@stop
+	@stop
