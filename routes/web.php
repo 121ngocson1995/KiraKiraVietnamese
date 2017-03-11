17241379cf5fbb8afc3f8c84b5b19860b695c685
121ngocson1995 <super_musix@yahoo.com.vn>
2017-03-11 13:23:01 +0700

    uncomment dummy route

--- routes/web.php
+++ routes/web.php
@@ -30,19 +30,19 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('/dummy', 'DummyController@load');
 // Route::get('/dummy1', 'DummyController@load');		// Example
-Route::get('/lesson{lessonNo}/{activity}', 'DummyController@load');
-Route::get('/lesson1/P1', 'P1Controller@load');
-Route::get('/lesson1/P2', 'P2Controller@load');
-Route::get('/lesson1/P3', 'P3Controller@load');
-Route::get('/lesson1/P6', 'P6Controller@load');
-Route::get('/lesson1/P8', 'P8Controller@load');
-Route::get('/lesson1/P10', 'P10Controller@load');
-Route::get('/lesson1/P11', 'P11Controller@load');
-Route::get('/lesson1/P12', 'P12Controller@load');
-Route::get('/lesson1/P13', 'P13Controller@load');
-Route::get('/lesson1/P14', 'P14Controller@load');
-Route::get('/lesson1/Expansion', 'ExtendController@load');
-Route::get('/lesson1/Situation', 'SituController@load');
+// Route::get('/lesson{lessonNo}/{activity}', 'DummyController@load');
+// Route::get('/lesson1/P1', 'P1Controller@load');
+// Route::get('/lesson1/P2', 'P2Controller@load');
+// Route::get('/lesson1/P3', 'P3Controller@load');
+// Route::get('/lesson1/P6', 'P6Controller@load');
+// Route::get('/lesson1/P8', 'P8Controller@load');
+// Route::get('/lesson1/P10', 'P10Controller@load');
+// Route::get('/lesson1/P11', 'P11Controller@load');
+// Route::get('/lesson1/P12', 'P12Controller@load');
+// Route::get('/lesson1/P13', 'P13Controller@load');
+// Route::get('/lesson1/P14', 'P14Controller@load');
+// Route::get('/lesson1/Expansion', 'ExtendController@load');
+// Route::get('/lesson1/Situation', 'SituController@load');
 /*
 |--------------------------------------------------------------------------
 | End dummy routes
