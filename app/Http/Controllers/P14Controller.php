20a29b184f04396145edb4eb25c367c174d5b806
minhbeo2805 <nguyenleminh2805@gmail.com>
2017-03-12 21:22:42 +0700

    update P14controller

--- app/Http/Controllers/P14Controller.php
+++ app/Http/Controllers/P14Controller.php
@@ -9,29 +9,33 @@ class P14Controller extends Controller
 {
     public function load()
     {
-    	// dummy course và lesson
-    	$course_id= 1;
-		$lesson_id= 1;
+        // dummy course và lesson
+        $course_id= 1;
+        $lesson_id= 1;
 
-		// Lấy dữ liệu từ db
-    	$elementData = P14Element::where('lesson_id', '=', $lesson_id)->get();
-    	$cnt = count($elementData);
-    	$nounArr = array();
-    	$clauseArr = array();
+        // Lấy dữ liệu từ db
+        $elementData = P14Element::where('lesson_id', '=', $lesson_id)->get();
+        $cnt = count($elementData);
+        $nounArr = array();
+        $clauseArr = array();
 
-    	if ($cnt != 0){
-    		for ($i=0; $i<$cnt; $i++){
-    			$nounArr1[$i] = explode( "{", $elementData[$i]->sentence);
-    		}
+        if ($cnt != 0){
+            for ($i=0; $i<$cnt; $i++){
+                $clauseArr[$i] = explode( "*", $elementData[$i]->sentence);
+            }
+            for ($i=0; $i<$cnt; $i++){
+                $open[$i] = $clauseArr[$i][0];
+                $nounArr[$i] = explode("|", $clauseArr[$i][1]);
+                $close[$i] = $clauseArr[$i][2];
+                // $nounArr[$i] = $clauseArr[$i][1];
+            }
 
-    		//dd($nounArr1);
+            return view("P14", compact(['elementData', 'open', 'nounArr', 'close', 'cnt'])); 
+        } else {
+            return view("P14", compact('elementData'));
+        }
 
-    		return view("P14", compact(['elementData', 'dialogCnt'])); 
-    	} else {
-    		return view("P14", compact(['elementData', 'dialogCnt']));
-    	}
-
-    	
-    	
+        
+        
     }
 }
