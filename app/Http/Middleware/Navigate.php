ebf7b85d0a9705f84d9f7c44c98d3d4e983eff09
minhbeo2805 <nguyenleminh2805@gmail.com>
2017-03-23 15:35:18 +0700

    update navitgate

--- app/Http/Middleware/Navigate.php
+++ app/Http/Middleware/Navigate.php
@@ -15,37 +15,39 @@ class Navigate
      */
     public function handle($request, Closure $next)
     {   
-        // $lessons = $request->get('lessons');
-        // $lessonNo = substr_replace($request->get('lessonNo'),"", 0, 6);
-        // $activity = $request->get('activity');
-        // $lessonAct;
-        // $preAct;
-        // $nextAct;
+        $lessons = $request->get('lessons');
+        $lessonNo = substr_replace($request->get('lessonNo'),"", 0, 6);
+        $activity = $request->get('activity');
+        $lessonAct;
+        $preAct;
+        $nextAct;
 
-        // for ($i=0; $i < count($lessons); $i++) { 
-        //     if ($lessons[$i]->lessonNo == $lessonNo) {
-        //         $lessonAct = $lessons[$i]->activity;
-        //     }
-        // }
-        // for ($i=0; $i < count($lessonAct); $i++) { 
-        //     if ($lessonAct[$i]->name == $activity) {
-        //         if ($i == 0) {
-        //             $preAct = false;
-        //             return;
-        //         }else{
-        //             $preAct = $lessonAct[$i-1];
-        //         }
+        for ($i=0; $i < count($lessons); $i++) { 
+            if ($lessons[$i]->lessonNo == $lessonNo) {
+                $lessonAct = $lessons[$i]->activity;
+            }else{
+                return $next($request);
+            }
+        }
+        for ($i=0; $i < count($lessonAct); $i++) { 
+            if ($lessonAct[$i]->name == $activity) {
+                if ($i == 0) {
+                    $preAct = false;
+                    return;
+                }else{
+                    $preAct = $lessonAct[$i-1];
+                }
 
-        //         if ($i == count($lessonAct)-1) {
-        //             $nextAct = false;
+                if ($i == count($lessonAct)-1) {
+                    $nextAct = false;
                     
-        //         }else{
-        //             $nextAct = $lessonAct[$i+1];
-        //         }
-        //     }
-        // }
+                }else{
+                    $nextAct = $lessonAct[$i+1];
+                }
+            }
+        }
         
-        // $request->attributes->add(['preAct' => $preAct, 'nextAct' => $nextAct]);
+        $request->attributes->add(['preAct' => $preAct, 'nextAct' => $nextAct]);
         return $next($request);
     }
 }
