bc5852901cd68f0dd6fc2f6f1d27f8cec23a896b
minhbeo2805 <nguyenleminh2805@gmail.com>
2017-03-31 10:39:28 +0700

    update p5 + db

---
+++ database/sql/homestead_p5_dialogue_memorizes.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: homestead
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.19-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `p5_dialogue_memorizes`
+--
+
+LOCK TABLES `p5_dialogue_memorizes` WRITE;
+/*!40000 ALTER TABLE `p5_dialogue_memorizes` DISABLE KEYS */;
+INSERT INTO `p5_dialogue_memorizes` VALUES (1,1,0,'- Chào bạn!|- Chào bạn!','audio/P5/audio_1_0.mp3',NULL,NULL,NULL),(2,1,1,'- Chào chị!|- Chào em!','audio/P5/audio_1_1.mp3',NULL,NULL,NULL),(3,1,2,'- Chào anh!|- Chào em!','audio/P5/audio_1_2.mp3',NULL,NULL,NULL),(4,1,3,'- Chào chú!|- Chào cháu!','audio/P5/audio_1_3.mp3',NULL,NULL,NULL),(5,1,4,'- Chào cô!|- Chào cháu!','audio/P5/audio_1_4.mp3',NULL,NULL,NULL),(6,1,5,'- Chào bác!|- Chào cháu!','audio/P5/audio_1_5.mp3',NULL,NULL,NULL),(7,1,6,'- Bạn tên là gì ?|- Tôi tên là Trung anh.','audio/P5/audio_1_6.mp3',NULL,NULL,NULL),(8,1,7,'- Cháu tên là gì?|- Cháu tên là Trung Anh.','audio/P5/audio_1_7.mp3',NULL,NULL,NULL),(9,1,8,'- Bạn mấy tuổi ?|- Tôi 7 tuổi.','audio/P5/audio_1_8.mp3',NULL,NULL,NULL),(10,1,9,'- Em bao nhiêu tuổi ?|- Em 7 tuổi ạ.','audio/P5/audio_1_9.mp3',NULL,NULL,NULL),(11,1,10,'- Bạn là hoc sinh à ?|- Ừ, tôi là học sinh.','audio/P5/audio_1_10.mp3',NULL,NULL,NULL),(12,1,11,'- Cháu là học sinh à ?|-Vâng ạ, cháu là học sinh.','audio/P5/audio_1_11.mp3',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `p5_dialogue_memorizes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-31 10:39:08
--- resources/views/activities/P5.blade.php
+++ resources/views/activities/P5.blade.php
@@ -1,33 +1,118 @@
 @extends('activities.layout.activityLayout')
 
 @section('actContent')
- <hr>
- <script type="text/javascript">
- 	function playWord(audioPath) {
+<hr>
+<style type="text/css">
+	#record-content{
+		width: 230px;
+		height: 60px;
+		position: fixed;
+		z-index: 999;
+		right: 130px;
+		bottom: 40%;
+	}
+</style>
+<script type="text/javascript">
+	function playWord(audioPath) {
 		var auRecord = document.getElementById("sample");
 
 		auRecord.src = audioPath;
 		auRecord.play();
 	} 
- </script>
- 
- <div class="row">
- 	<div id="content_id" class="col-sm-9 col-md-6 col-lg-8">
- 	<audio id="sample"></audio>
- 		@for ($i = 0; $i < count($contentArr) ; $i++)
- 		<div class="row">
- 			<div class="col-sm-6 col-md-6 col-lg-6" align="right">
- 				@for ($j = 0; $j < count($contentArr[$i]) ; $j++)
- 				<p align="center" >{{ $contentArr[$i][$j]}}</p>
- 				@endfor
- 			</div>
- 			<div class="col-sm-6 col-md-6 col-lg-6" align="left">
- 			<input type="image" class="controlBtn" src="{{ asset('img/icons/sample_play.svg') }}" width="10%" height="10%" onclick="playWord('{{ URL::asset($audioArr[$i]) }}')">
- 			</div>
- 		</div>
- 		<hr>
- 		@endfor
- 	</div>
- </div>
+</script>
+<div id="content_id" class="col-sm-9 col-md-6 col-lg-8">
+	<audio id="sample"></audio>
+	@for ($i = 0; $i < count($contentArr) ; $i++)
+	<div class="row">
+		<div class="col-sm-6 col-md-6 col-lg-6" align="right">
+			@for ($j = 0; $j < count($contentArr[$i]) ; $j++)
+			<p align="center" >{{ $contentArr[$i][$j]}}</p>
+			@endfor
+		</div>
+		<div class="col-sm-6 col-md-6 col-lg-6" align="left">
+			<input type="image" class="controlBtn" src="{{ asset('img/icons/sample_play.svg') }}" width="10%" height="10%" onclick="playWord('{{ URL::asset($audioArr[$i]) }}')">
+		</div>
+	</div>
+	<hr>
+	@endfor
+</div>
+<div id="record-content">
+	<input type="image" class="startRecord controlBtn" style="width: 100px; height: 100px;" id="btn-rec" src="{{ asset('img/icons/rec_startRecording.svg') }}" onclick="startRecording(this); /*startProgress('progressRecord')*/" alt="Submit" width="130" height="130">
+	<input type="image" class="controlBtn" style="width: 100px; height: 100px;" id="btn-playRec" src="{{ asset('img/icons/rec_playback2.svg') }}" onclick="$('#auRecord')[0].pause(); $('#auRecord')[0].currentTime = 0; $('#auRecord')[0].play(); /*startProgress('progressPlayback')*/" alt="Submit" width="130" height="130" data-toggle="tooltip" data-placement="bottom" title="Click the middle button to record your voice!">
+	<audio id="auRecord"></audio>
+</div>
+<script src="{{ asset('js/recorder.js') }}"></script>
+<script type="text/javascript">
+	window.onload = function init() {
+		try {
+			// webkit shim
+			window.AudioContext = window.AudioContext || window.webkitAudioContext;
+			navigator.getUserMedia = ( navigator.getUserMedia ||
+				navigator.webkitGetUserMedia ||
+				navigator.mozGetUserMedia ||
+				navigator.msGetUserMedia);
+			window.URL = window.URL || window.webkitURL;
 
- @stop
+			audio_context = new AudioContext;
+			console.log('Audio context set up.');
+			console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+		} catch (e) {
+			alert('No web audio support in this browser!');
+		}
+
+
+		navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+			window.alert('No live audio input: ' + e);
+		});
+
+		$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip("show");   
+		});
+	}
+	function startUserMedia(stream) {
+		var input = audio_context.createMediaStreamSource(stream);
+
+		recorder = new Recorder(input);
+	}
+
+	function startRecording(button) {
+		muteSound()
+		recorder && recorder.record();
+		$(button).removeClass('startRecord');
+		$(button).addClass('stopRecord');
+		$(button).attr('src', '{{ asset('img/icons/rec_stopRecording.svg') }}');
+
+		setTimeout(function() {
+			if ($(button).hasClass('stopRecord')) {
+				recorder && recorder.stop();
+				$(button).removeClass('stopRecord');
+				$(button).addClass('startRecord');
+				$(button).attr('src', '{{ asset('img/icons/rec_startRecording.svg') }}');
+
+				createMedia($(button).attr("id"));
+
+				recorder.clear();
+			}
+		}, 3000);
+	}
+
+	
+	function createMedia() {
+		recorder && recorder.exportWAV(function(blob) {
+			var url = URL.createObjectURL(blob);
+			var auRecord = document.getElementById("auRecord");
+
+			auRecord.src = url;
+			localStorage.setItem("record", url);
+		});
+	}
+
+	function muteSound(){
+		$('audio').each(function() {
+			this.pause();
+			this.currentTime = 0;
+			$(this).unbind();
+		})
+	}
+</script>
+@stop
