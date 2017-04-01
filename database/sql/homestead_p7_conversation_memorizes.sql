16cdf73b756e4de7b534032e8a3f63ef0c020f04
minhbeo2805 <nguyenleminh2805@gmail.com>
2017-04-01 12:28:19 +0700

    db ms cho P7

---
+++ database/sql/homestead_p7_conversation_memorizes.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: homestead
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.19-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `p7_conversation_memorizes`
+--
+
+LOCK TABLES `p7_conversation_memorizes` WRITE;
+/*!40000 ALTER TABLE `p7_conversation_memorizes` DISABLE KEYS */;
+INSERT INTO `p7_conversation_memorizes` VALUES (0,1,3,'Trung Anh :* - Chào anh.|Ba :* - Chào em.| *Em tên là gì?|Trung Anh:* - Em tên là Trung Anh ạ.|Ba :* - Em mấy tuổi rồi?|Trung Anh :* - Em 9 tuổi ạ.|Ba :* - Em là học sinh à?|Trung Anh :* - Vâng ạ, cháu là học sinh ạ.','audio/P7/lesson1/audio_3.mp3',NULL,NULL,NULL),(1,1,0,'Trung Anh :* - Chào bạn.|Hoa :* - Chào bạn.|Trung Anh :* - Tôi tên là Trung Anh.|Hoa :* - Tôi tên là Hoa.|Trung Anh :* - Tôi chín tuổi.|Hoa :* - Tôi cũng chín tuổi.','audio/P7/lesson1/audio_0.mp3',NULL,NULL,NULL),(2,1,1,'Trung Anh :* - Chào bạn.|Hoa :* - Chào bạn.|Trung Anh :* - Tôi tên là Trung Anh, còn bạn?|Hoa :* - Tôi à, tôi tên là Hoa.| * Tôi là học sinh, còn bạn?|Trung Anh :* - Tôi cũng là học sinh.','audio/P7/lesson1/audio_1.mp3',NULL,NULL,NULL),(3,1,2,'Trung Anh :*  - Chào bạn.|Hoa :* - Chào bạn.|Trung Anh :* - Tôi tên là Trung Anh, còn bạn?|Hoa :* - Tôi tên là Hoa.| * Tôi chín tuổi, còn bạn?|Trung Anh :* - Tôi cũng chín tuổi.| * Tôi là học sinh.','audio/P7/lesson1/audio_2.mp3',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `p7_conversation_memorizes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-01 12:27:06
