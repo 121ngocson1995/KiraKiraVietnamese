8fc9c6d62272dcfb6727cd40a587e2103ea8c9e7
Sơn <super_musix@yahoo.com.vn>
2017-04-13 07:01:35 +0700

    Update

--- resources/views/activities/P13.blade.php
+++ resources/views/activities/P13.blade.php
@@ -3,16 +3,41 @@
 @section('actContent')
 
 <link rel="stylesheet" href="{{ asset('P13_assets/css/styles.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/KiraNav.css') }}">
 
 <style>
 	body {
 		background: url({{ asset('P13_assets/img/p13.svg') }}) no-repeat center center fixed;
 		background-size: cover;
 	}
+	article {
+		padding-top: 5%;
+	}
+	blockquote {
+		float: none;
+		padding: 0;
+		border-top: initial;
+		border-bottom: initial;
+	}
+	.container2:before {
+		transform: translateY(-30%);
+	}
+	.container2:after {
+		transform: translateY(10%);
+	}
+	#promo2 {
+		padding-top: 0;
+	}
 </style>
 
 <div class="prac-grey-section">
+
+	<article class="container2">
+		<blockquote>
+			@foreach ($elementData as $value)
+			{{ $value->content }}
+			@endforeach
+		</blockquote>
+	</article>
 
 	<div id="promo2">
 		<div class="jumbotron practice">
@@ -27,24 +52,15 @@
 			</div>
 		</div>
 	</div>
-
-
-	<article class="container2">
-		<blockquote>
-			@foreach ($elementData as $value)
-			{{ $value->content }}
-			@endforeach
-		</blockquote>
-	</article>
 </div>
 
 
 @stop
 
 @section('actDescription-vi')
-
+	Học thuộc lòng bài đọc và làm theo hướng dẫn.
 @stop
 
 @section('actDescription-en')
-  
+	Memorize the given texts and follow the guide.
 @stop
