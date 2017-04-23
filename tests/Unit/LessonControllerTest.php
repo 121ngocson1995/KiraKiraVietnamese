e16c191c0b884488cb24e724337bd8d0ec80c073
Sơn <super_musix@yahoo.com.vn>
2017-04-24 05:56:13 +0700

    test

--- tests/Unit/LessonControllerTest.php
+++ tests/Unit/LessonControllerTest.php
@@ -13,6 +13,7 @@ class LessonControllerTest extends TestCase
 	{
 		$course = factory(\App\Course::class, 1)->create();
 
+		parent::setUp();
 		$lesson = \App\Lesson::create([
 			'course_id' => $course->id,
 			'lessonNo' => 1,
