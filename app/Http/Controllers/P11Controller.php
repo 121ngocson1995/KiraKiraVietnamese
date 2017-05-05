874e7b8e124c87ba9ad287a002920e123e9e2e26
minhbeo2805 <nguyenleminh2805@gmail.com>
2017-05-05 08:02:56 +0700

    fix

--- app/Http/Controllers/P11Controller.php
+++ app/Http/Controllers/P11Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Lesson;
 use Illuminate\Http\Request;
 use App\P11ConversationReorder;
+use Illuminate\Support\Facades\Validator;
 
 class P11Controller extends Controller
 {
